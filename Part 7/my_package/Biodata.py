2d8f8938e4d45c0bb9c3dc041ece90e5e865f14e
Arif Su'admaji <104288379+arifsuadmaji@users.noreply.github.com>
2023-11-02 06:15:52 +0700

    Add files via upload

---
+++ Part 7/my_package/Biodata.py
@@ -0,0 +1,2 @@
+def Biodata(nama, jenis_kelamin, alamat):
+    print('Nama : ', nama, '\njenis Kelamin : ', jenis_kelamin, '\nNama : ', alamat)
---
+++ Part 7/my_package/bangun_ruang.py
@@ -0,0 +1,11 @@
+# bangun_ruang.py
+def volume_Kubus(panjang_sisi):
+    return panjang_sisi**3
+def volume_bola(r):
+    return (4/3)*(22/7)*(r**3)
+def volume_balok(p, l, t):
+    return p*l*t
+def volume_tabung(r, t):
+    return (22/7)*(r**2)*t
+def volume_kerucut(r, t):
+    return (1/3)*(22/7)*(r**2)*t
